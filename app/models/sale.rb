062b5ab6d4fb364475e10db377fb3f06ff065f62
serra <serra@modemgirl.ca>
2022-07-11 18:36:56 -0700

    add sale helper

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,23 @@
 class Sale < ApplicationRecord
+
+  # this is an ACTIVE RECORD SCOPE
+  # aka a class method
+  # we can create as many of these as we want to look up stuff about sales
+  # and now we can reference sales.active.any in the sales_helper file to check for active sales
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
+
+  def finished? 
+    ends_on < Date.current  
+  end
+
+  def upcoming? 
+    starts_on > Date.current
+  end
+
+  def active? 
+   !upcoming? && !finished? 
+  end
+
 end
